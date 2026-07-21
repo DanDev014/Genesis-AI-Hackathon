3739d3a55b239b09da635f936e515d4579fd3421
rovebelabanga <rovenelabanga2001@gmail.com>
2026-07-21 23:50:35 +0300

    refactor: summary model

--- backend/app/models/summary.py
+++ backend/app/models/summary.py
@@ -9,33 +9,10 @@ class Summary(db.Model):
         primary_key=True,
     )
 
-    user_id = db.Column(
-        db.Integer,
-        db.ForeignKey("users.user_id"),
-        nullable=False,
-    )
-
     client_id = db.Column(
         db.Integer,
         db.ForeignKey("clients.client_id"),
         nullable=False,
-    )
-
-    proposal_id = db.Column(
-        db.Integer,
-        db.ForeignKey("proposals.proposal_id"),
-        nullable=True,
-    )
-
-    quote_id = db.Column(
-        db.Integer,
-        db.ForeignKey("quotes.quote_id"),
-        nullable=True,
-    )
-
-    quote = db.relationship(
-       "Quote",
-        back_populates="summary",
     )
 
     first_meeting_deliverables = db.Column(
@@ -58,8 +35,6 @@ class Summary(db.Model):
             "summary_id": self.summary_id,
             "user_id": self.user_id,
             "client_id": self.client_id,
-            "proposal_id": self.proposal_id,
-            "quote_id": self.quote_id,
             "first_meeting_deliverables": self.first_meeting_deliverables,
             "created_at": (
                 self.created_at.isoformat()
